550d24a15870676dd17f2c97a43779094c4f5bd0
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-07-25 14:12:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,18 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
+variable "db_username" {
+  type = string
+  default = "admin"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "key_name" {
+  description = "EC2 Key pair name"
+  type        = string
+}
+
