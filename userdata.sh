e6fa7e71438e494a440990e5df6d34824ab33862
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-07-25 13:34:45 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras enable php8.0
+amazon-linux-extras install php8.0 -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<?php phpinfo(); ?>" > /var/www/html/index.php
 
